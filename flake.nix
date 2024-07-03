cc77f23187dbcf450d67832a448cf25076f2ed90
Robin Stumm <serverkorken@gmail.com>
2024-07-03 13:34:41 +0200

    update to release 24.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
-    # needs proot 5.3.0 that fixes crashes
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
     flake-utils.url = github:numtide/flake-utils;
     home-manager = {
-      url = github:nix-community/home-manager/release-23.11;
+      url = github:nix-community/home-manager/release-24.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
